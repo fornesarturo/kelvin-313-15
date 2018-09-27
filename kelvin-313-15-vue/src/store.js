9223d76df088cb30420e79b64a03f737af210a74
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2018-09-27 12:11:14 -0500

    Router and store

---
+++ kelvin-313-15-vue/src/store.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import createPersistedState from 'vuex-persistedstate'
+
+const vuexLocalStorage = createPersistedState({
+    key: 'kelvin-local-storage'
+})
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+
+    },
+    actions: {
+
+    },
+    mutations: {
+
+    },
+    getters: {
+
+    },
+    plugins: {
+        vuexLocalStorage
+    }
+})
