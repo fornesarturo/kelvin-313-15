9c0cbaab4d7ee7550281f50be4deb053752156c5
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2018-09-27 16:32:40 -0500

    Start club info

--- kelvin-313-15-vue/src/components/Clubs/ClubDetail/ClubInfo.vue
+++ kelvin-313-15-vue/src/components/Clubs/ClubDetail/ClubInfo.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <div>
+    <h2>Leyendo: {{ title }}</h2>
+  </div>
 </template>
 
 <script>
 export default {
-    
+  props: {
+    title: {
+      type: String,
+      default: 'No hay libro seleccionado'
+    }
+  }
 }
 </script>
 
