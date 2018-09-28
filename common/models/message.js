057a6f2c2bbd5c52ce91d4a2d7e00f3f150766df
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2018-09-28 12:06:54 -0500

    Trying too hard

--- common/models/message.js
+++ common/models/message.js
@@ -2,4 +2,29 @@
 
 module.exports = function(Message) {
 
+  Message.getMessages = function (clubId, bookId, cb) {
+    if (clubId && bookId) {
+      Message.find({where: {and: [{clubId: clubId}, {bookId: bookId}]}}, function (err, messages) {
+        if (err) return cb(err);
+        cb(null, messages);
+      });
+    } else if (bookId) {
+      Message.find({where: [{bookId: bookId}, {clubId: null}]}, function (err, messages) {
+        if (err) return cb(err);
+        cb(null, messages);
+      });
+    } else {
+      return cb({status:400, message:'Unavailable service'});
+    }
+  }
+
+  Message.remoteMethod(
+    'getMessages', {
+      'accepts': [
+        { arg: 'clubId', type: 'string', required: false },
+        { arg: 'bookId', type: 'string', required: false } 
+      ],
+      'returns': { arg: 'data', type: 'object' }
+    }
+  )
 };
