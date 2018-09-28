b649a3f2d0c0a5ff142222f3bb166c5d7e0db2e1
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2018-09-28 12:04:53 -0500

    Recomented

--- client/src/services.js
+++ client/src/services.js
@@ -3,8 +3,11 @@ import axios from 'axios'
 const BASE_URL = "localhost:3000/api/"
 
 export default {
-  getClubs (email) {
-
+  getMessages: ({ clubId, bookId }) => {
+    return axios.post(`${BASE_URL}/Messages/getMessages`, { 'clubId': clubId, 'bookId': bookId })
+  },
+  addMessage: ({ clubId, bookId, author, message }) => {
+    return axios.post(`${BASE_URL}/Messages`, { 'text': message, 'author': author, 'clubId': clubId, 'bookId': bookId });
   },
   getUser (email) {
 
